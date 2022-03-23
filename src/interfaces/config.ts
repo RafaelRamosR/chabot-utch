77d52f569b8a329533c7b8f7a594e81ae55aab25
Rafael Ramos <rafael.ramos@energiasolarsa.net>
2022-03-23 16:29:30 -0500

    Dialog flow environment

--- src/interfaces/config.ts
+++ src/interfaces/config.ts
@@ -3,3 +3,16 @@ export interface IApiConfig {
   API_VERSION: string;
   PORT: number;
 }
+
+export interface ITwilioConfig {
+  ACCOUNT_SID: string;
+  AUTH_TOKEN: string;
+  TWILIO_NUMBER_PHONE: string;
+}
+
+export interface IDialogFlowConfig {
+  DF_LANGUAGE_CODE: string;
+  GOOGLE_CLIENT_EMAIL: string;
+  GOOGLE_PRIVATE_KEY: string;
+  GOOGLE_PROJECT_ID: string;
+}
