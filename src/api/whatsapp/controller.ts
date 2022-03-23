6eacca3a094bf1a5685f8e55edfdbbd799dd833e
Rafael Ramos <rafael.ramos@energiasolarsa.net>
2022-03-23 17:17:57 -0500

    Response message with the same message

--- src/api/whatsapp/controller.ts
+++ src/api/whatsapp/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-// import { sendTextMessage } from '../../services/twilio';
+import { sendTextMessage } from '../../services/twilio';
+// import { sendToDialogFlow } from '../../services/dialogFlow';
 
 export async function whatsappSendMessage(
   request: Request,
@@ -7,7 +8,11 @@ export async function whatsappSendMessage(
   next: NextFunction
 ) {
   try {
-    console.log('req -> ', request.body);
+    const { Body, WaId } = request.body;
+    const result = await sendTextMessage(WaId, Body);
+    console.log('Result: ', result);
+
+    response.json({ ok: 200 });
   } catch (error) {
     return next(error);
   }
