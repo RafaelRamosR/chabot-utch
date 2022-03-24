c0d8b379fdf60e0743efb1941d6dcaa7e18bf1f9
Rafael Ramos <rafael.ramos@energiasolarsa.net>
2022-03-24 14:21:05 -0500

    Response with store data

--- src/api/whatsapp/controller.ts
+++ src/api/whatsapp/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { sendTextMessage } from '../../services/twilio';
 import { sendToDialogFlow } from '../../services/dialogFlow';
+import { getConnection } from '../../db';
 
 export async function whatsappSendMessage(
   request: Request,
@@ -11,8 +12,14 @@ export async function whatsappSendMessage(
     const { Body, WaId } = request.body;
     const { fulfillmentText } = await sendToDialogFlow(Body);
 
-    const result = await sendTextMessage(WaId, fulfillmentText);
-    console.log('Result: ', result);
+    const { messages } = getConnection()
+      .get('info')
+      .find({ code: fulfillmentText })
+      .value();
+
+    for (const message of messages) {
+      await sendTextMessage(WaId, message.replace(/\\n/g, '\n'));
+    }
 
     response.json({ ok: 200 });
   } catch (error) {
